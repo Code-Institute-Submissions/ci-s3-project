2051dbdc2fb919d177879a2b10aab48aaabb9874
Tati <tatiestrada@me.com>
2017-05-19 16:21:16 +0100

    working fine

--- cannedair/settings.py
+++ cannedair/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -132,7 +133,7 @@ USE_TZ = True
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
-ALLOWED_HOSTS = ['breatheworld.herokuapp.com']
+ALLOWED_HOSTS = ['breatheworld.herokuapp.com', '127.0.0.1']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
